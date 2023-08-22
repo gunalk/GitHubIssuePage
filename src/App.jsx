62384a5939c4682c8eaa4b259f26349283a0f1de
gunalk <lfgunal060@gmail.com>
2023-08-22 07:59:12 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,30 @@ import IssuesContainer from './Components/IssuesContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageNo from './Components/PageNo';
 import { GoTriangleDown } from "react-icons/go";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+
 
 
 function App() {
+
   const [issues,setIssues] = useState([]);
   const [loading, setLoading] = useState(false);
   const[input,setInput]=useState("")
   const startingPage=useSelector((state)=>state.user.currentpage)
   const [IssuesEachPage] = useState(10);
+  const token=process.env.TOKEN
 
+  
+ 
+  
   const fetchIssues = async () => {
     setLoading(true)
     const octokit = new Octokit({
-<<<<<<< HEAD
-      auth:"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"    })
-=======
-      auth: "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"    })
->>>>>>> 70ae826ee98c4564d4b8697999c18cdc0da3f269
+      auth: {token}})
     let response = await octokit.request('GET /repos/prettier/prettier/issues', {
       owner: 'prettier',
       repo: 'prettier',
@@ -43,6 +50,7 @@ function App() {
 
   
 
+  
   return (
     <div className='container mt-5'>
       <div id='Input-field' >
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import {store} from "./store/store"
 import {Provider} from "react-redux"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
     <App />
